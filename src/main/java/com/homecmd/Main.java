914de97008240f1df15c084ef506eb6d55cbfa4a
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2016-08-15 16:58:12 +0900

    Create New Project

---
+++ src/main/java/com/homecmd/Main.java
@@ -0,0 +1,17 @@
+package com.homecmd;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	public static JavaPlugin plugin;
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
