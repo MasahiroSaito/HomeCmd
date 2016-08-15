197cd9287e0e1f5587509cd3d15eabed41eea8bb
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2016-08-15 17:12:39 +0900

    スペースを削除

--- src/main/java/com/homecmd/Main.java
+++ src/main/java/com/homecmd/Main.java
@@ -4,14 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	public static JavaPlugin plugin;
-	
+
 	@Override
 	public void onEnable() {
-		
+
 	}
-	
+
 	@Override
 	public void onDisable() {
-		
+
 	}
 }
